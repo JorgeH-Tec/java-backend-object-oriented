8ebd636dcf7a19349045849e7a40e9a6ac789a82
Jorge Henrique <jorge.paiv4@gmail.com>
2026-05-12 15:58:06 -0300

    feat(inheritance/): add features for investor class

--- 02-Heranca/java-poo-inheritance/src/Investor.java
+++ 02-Heranca/java-poo-inheritance/src/Investor.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Investor {
+    // Attributes
+    private String nome;
+    private final List<Asset> portfolio = new ArrayList<>();
+
+    // Constructor
+    public Investor(String nome) {
+        setNome(nome);
+    }
+
+    // public Methods
+    public void addAsset (Asset asset) {
+        portfolio.add(asset);
+    }
+
+    public void showGeneralReport() {
+        for (Asset asset : portfolio) {
+            asset.exibirRelatorio();
+        }
+    }
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Erro, o nome não pode ser vazio nem nulo.");
+        }
+        this.nome = nome;
+    }
+
+    public List<Asset> getPortfolio() {
+        return portfolio;
+    }
 }
